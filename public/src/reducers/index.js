5df28119e52a4f025fa250c736939092b0fcaf24
gamaliel <fabiogamaliel@gmail.com>
2017-07-05 16:29:34 -0400

    importando el reductor de project

--- public/src/reducers/index.js
+++ public/src/reducers/index.js
@@ -1,9 +1,11 @@
 import { combineReducers } from 'redux'
 
 import postsReducer from '../post/reducer'
+import projectsReducer from '../project/reducer'
 
 const rootReducer = combineReducers({
-  posts: postsReducer
+  posts: postsReducer,
+  projects: projectsReducer
 })
 
 export default rootReducer
